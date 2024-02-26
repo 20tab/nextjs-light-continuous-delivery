7dbb664fc920947a373bac97fd623e3c352521d7
lucaspinosi <luca.spinosi@20tab.com>
2024-02-26 15:27:58 +0100

    TG-1136 Utilize remote.Dockerfile to generate the robot.txt

--- {{cookiecutter.project_dirname}}/prebuild.js
+++
@@ -1,9 +0,0 @@
-const generateRobotsTxt = require('./generate-robot-txt')
-
-generateRobotsTxt()
-
-console.log(
-    `Generated a ${
-      process.env.REACT_ENVIRONMENT === "production" ? "crawlable" : "non-crawlable"
-    } public/robots.txt`
-);
